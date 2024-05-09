dc76540ff78705f5410ab26267b9f5b8eb61d524
Justin Yoo <justin.yoojh@gmail.com>
2024-05-09 18:02:08 +0900

    Update API registration part (#13)

---
+++ infra/hooks/postprovision.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Runs the post-provision script after the environment is provisioned
+# It does the following:
+#   1. Creates a service principal
+#   2. Adds required permissions to the app
+#   3. Sets the environment variables
+
+set -e
+
+echo "Running post-provision script..."
+
+REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
+
+# Run only if GITHUB_WORKSPACE is NOT set - this is NOT running in a GitHub Action workflow
+if [ -z "$GITHUB_WORKSPACE" ];
+then
+    echo "Registering the application in Azure..."
+
+    # Load the azd environment variables
+    "$REPOSITORY_ROOT/infra/hooks/load_azd_env.sh"
+
+    # Create a service principal and assign the required permissions
+    appId=$AZURE_CLIENT_ID
+    if [ -z "$appId" ]
+    then
+        appId=$(az ad app list --display-name "spn-$AZURE_ENV_NAME" --query "[].appId" -o tsv)
+        if [ -z "$appId" ]
+        then
+            appId=$(az ad app create --display-name "spn-$AZURE_ENV_NAME" --query "appId" -o tsv)
+            spnId=$(az ad sp create --id $appId --query "id" -o tsv)
+        fi
+    fi
+
+    spnId=$(az ad sp list --display-name "spn-$AZURE_ENV_NAME" --query "[].id" -o tsv)
+    if [ -z "$spnId" ]
+    then
+        spnId=$(az ad sp create --id $appId --query "id" -o tsv)
+    fi
+
+    objectId=$(az ad app show --id $appId --query "id" -o tsv)
+
+    # Add required permissions to the app
+    requiredResourceAccess="[{\"resourceAppId\": \"c3ca1a77-7a87-4dba-b8f8-eea115ae4573\", \"resourceAccess\": [{\"type\": \"Scope\", \"id\": \"44327351-3395-414e-882e-7aa4a9c3b25d\"}]}]"
+
+    payload=$(jq -n \
+    --argjson requiredResourceAccess "$requiredResourceAccess" \
+    "{\"requiredResourceAccess\": $requiredResourceAccess}")
+
+    az rest -m PATCH \
+        --uri "https://graph.microsoft.com/v1.0/applications/$objectId" \
+        --headers Content-Type=application/json \
+        --body "$payload"
+
+    # Set the environment variables
+    azd env set AZURE_CLIENT_ID $appId
+else
+    echo "Skipping to register the application in Azure..."
+fi
