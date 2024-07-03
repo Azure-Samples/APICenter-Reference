4711799b6bd6f5bf388d30efe75d2d0f4c1dcfcc
Julia Muiruri <julia.muiruri4@gmail.com>
2024-07-03 22:21:48 +0300

    chore: improved description

--- nodejs/webapi/app.js
+++ nodejs/webapi/app.js
@@ -56,12 +56,13 @@ app.use("/weatherforecast", weatherRouter);
 if (process.env.NODE_ENV !== 'production') {
   // route to serve basic/ improved JSON
   app.get("/api-docs/swagger.json", (req, res) => {
-    // res.send(swaggerSpecs.improved);
     res.send(swaggerSpecs.basic);
+    // res.send(swaggerSpecs.improved);
+    
   });
   
-  // setupSwaggerUi(app, swaggerSpecs.improved, "/api-docs/swagger");
   setupSwaggerUi(app, swaggerSpecs.basic, "/api-docs/swagger");
+  // setupSwaggerUi(app, swaggerSpecs.improved, "/api-docs/swagger");
 }
 
 app.get("/", (req, res) => {
